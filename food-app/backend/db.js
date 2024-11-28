537598cd30757613e43a95c7d2cd6db9774a5e2f
Ansh Jindal <anshjindal132@gmail,com>
2024-11-28 17:03:09 +0530

    schema create user

--- food-app/backend/db.js
+++ food-app/backend/db.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose');
-const MongoURI='mongodb+srv://anshjindal132:anshjinda@cluster0.q9ofh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoose = require("mongoose");
+const MongoURI =
+  "mongodb+srv://anshjindal132:anshjinda@cluster0.q9ofh.mongodb.net/GoFoods?retryWrites=true&w=majority&appName=Cluster0";
 const mongoDB = async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        mongoose.connect(MongoURI) 
-        console.log('Mongo connected')
-    }
-    catch(error) {
-        console.log(error)
-        process.exit()
-    }
-    }
-module.exports=mongoDB;
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(MongoURI);
+    console.log("Mongo connected");
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
+/*const fetch_data = mongoose.connection.db.collection("food_items");
+fetch_data.find({}).toArray(function (err, data) {
+  if (err) console.log(err);
+  else console.log(data);
+});*/
+module.exports = mongoDB;
--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -6,7 +6,10 @@ mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use(express.json());
+/*above is very imp*/
+app.use('/api',require("./routes/Createuser"))
+/*endpoint will be localhost:5000/api/createuser*/
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
