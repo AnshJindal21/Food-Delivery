662d312cec7fa58a8c33b394de38736b4d43d331
Ansh Jindal <anshjindal132@gmail,com>
2024-12-20 18:12:11 +0530

    cors issue

--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const mongoDB=require("./db")
+const cors = require('cors');
 mongoDB();
+
+app.use(cors({
+  origin: 'https://food-delivery-frontend-z9qv.onrender.com', // Allow only your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Include credentials if needed
+}));
+app.use(cors());
+app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://food-delivery-frontend-z9qv.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 /*app .use waale daalne hi hai*/
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
