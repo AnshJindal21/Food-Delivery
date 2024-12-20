7f9000b65525ae639a2f51f4929067a36d0253fb
Ansh Jindal <anshjindal132@gmail,com>
2024-12-20 17:05:50 +0530

    final project after features

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./screens/Home"
 import Login from "./screens/Login"
 import Signup from "./screens/Signup"
+import MyOrder from "./screens/MyOrder.js"
 import { CartProvider } from "./components/ContextReducer.jsx"
 
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css"
@@ -26,6 +27,7 @@ function App() {
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/login" element={<Login/>} />
         <Route exact path="/createuser" element={<Signup/>} />
+        <Route exact path="/myOrder" element={<MyOrder/>} />
 
         </Routes>
       </div>
